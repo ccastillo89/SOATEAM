e7b133ea65e429f5434706a164810f3fe4e93681
miguelmatosq <311miguelmatos@gmail.com>
2016-01-29 02:12:24 -0500

    SOAP v1.3.1

---
+++ UPC.SisTictecks/UPC.SisTictecks.SOAPGestionTicketsWS/IVehiculoService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using UPC.SisTictecks.EL;
+
+namespace UPC.SisTictecks.SOAPGestionTicketsWS
+{
+
+    [ServiceContract]
+    public interface IVehiculoService
+    {
+
+        #region "Vehiculo"
+
+        [FaultContract(typeof(RepetidoException))]
+        [OperationContract]
+        VehiculoEN CrearVehiculo(VehiculoEN vehiculoCrear);
+
+        [FaultContract(typeof(RepetidoException))]
+        [OperationContract]
+        VehiculoEN ObtenerVehiculo(int codigo);
+
+        [FaultContract(typeof(RepetidoException))]
+        [OperationContract]
+        VehiculoEN ModificarVehiculo(VehiculoEN vehiculoModificar);
+
+        [FaultContract(typeof(RepetidoException))]
+        [OperationContract]
+        bool EliminarVehiculo(int codigo);
+
+        [FaultContract(typeof(RepetidoException))]
+        [OperationContract]
+        List<VehiculoEN> ListarVehiculos();
+
+        #endregion
+
+
+        #region "Marca"
+
+        [FaultContract(typeof(RepetidoException))]
+        [OperationContract]
+        List<MarcaEN> ListarMarcas();
+
+        [FaultContract(typeof(RepetidoException))]
+        [OperationContract]
+        MarcaEN ObtenerMarca(int codigo);
+
+        #endregion
+
+        #region "Modelo"
+
+
+        [FaultContract(typeof(RepetidoException))]
+        [OperationContract]
+        List<ModeloEN> ListarModelos();
+
+        [FaultContract(typeof(RepetidoException))]
+        [OperationContract]
+        ModeloEN ObtenerModelo(int codigo);
+
+        #endregion
+
+        #region "Color"
+
+        [FaultContract(typeof(RepetidoException))]
+        [OperationContract]
+        List<ColorEN> ListarColores();
+
+        [FaultContract(typeof(RepetidoException))]
+        [OperationContract]
+        ColorEN ObtenerColor(int codigo);
+
+        #endregion
+
+    }
+}
